9db4d70a5bd143ee663d877996a08724e4ff0a50
Savan2711 <savananadani@gmail.com>
2019-01-30 21:12:40 +0530

    notification service

---
+++ showContact/app/src/main/java/app/com/example/andriod/showcontact/NotificationReceiver.java
@@ -0,0 +1,13 @@
+package app.com.example.andriod.showcontact;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class NotificationReceiver extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("yeah","yeah it works..");
+    }
+}
