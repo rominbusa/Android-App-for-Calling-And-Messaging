de22c9494b695465a71fe18bf5d552b8ddbf3ae5
Savan2711 <savananadani@gmail.com>
2019-03-15 04:35:14 +0530

    UI update of contact list

--- CallingAndMessaging/app/src/main/java/com/example/callingandmessaging/DisplayContactListActivity.java
+++ CallingAndMessaging/app/src/main/java/com/example/callingandmessaging/DisplayContactListActivity.java
@@ -3,6 +3,8 @@ package com.example.callingandmessaging;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +15,11 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class DisplayContactListActivity extends Activity {
+
+    private RecyclerView recyclerView;
+    private ContactListAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
@@ -20,32 +27,51 @@ public class DisplayContactListActivity extends Activity {
         ContactList contactList = (ContactList)getApplicationContext();
         final ArrayList<Person> arr = contactList.getPerson();
 
-        Log.d("arraylist size",String.valueOf(arr.size()));
-        String[] names =new String[arr.size()-1];
-        try{
-            for(int i = 0 ; i<arr.size() - 1 ; i++){
-                Log.d("names of " + i + "person is " , arr.get(i).getName());
-                if(arr.get(i).getName() != null) {
-                    names[i] = arr.get(i).getName();
-                }
-            }
-        }catch(NullPointerException e){
-            Log.d("Exceptions :","occures");
-        }
-        ArrayAdapter adapter=new ArrayAdapter<String>(this,R.layout.contact_detail,names);
-        final ListView listView = (ListView)findViewById(R.id.listview);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //for recyclerview
+        recyclerView = (RecyclerView) findViewById(R.id.contactListRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new ContactListAdapter(arr);
+        mAdapter.setOnItemClickListener(new ContactListAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public void onItemClick(int position) {
                 String selected_name = arr.get(position).getName();
                 Toast.makeText(DisplayContactListActivity.this, "You selected " + selected_name, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(DisplayContactListActivity.this, TimerActivity.class);
                 intent.putExtra("Selected_name", selected_name);
                 intent.putExtra("option", getIntent().getStringExtra("option"));
                 startActivity(intent);
-
             }
         });
+        recyclerView.setAdapter(mAdapter);
+
+        Log.d("arraylist size",String.valueOf(arr.size()));
+//        String[] names =new String[arr.size()-1];
+//        try{
+//            for(int i = 0 ; i<arr.size() - 1 ; i++){
+//                Log.d("names of " + i + "person is " , arr.get(i).getName());
+//                if(arr.get(i).getName() != null) {
+//                    names[i] = arr.get(i).getName();
+//                }
+//            }
+//        }catch(NullPointerException e){
+//            Log.d("Exceptions :","occures");
+//        }
+//        ArrayAdapter adapter=new ArrayAdapter<String>(this,R.layout.contact_detail,names);
+//        final ListView listView = (ListView)findViewById(R.id.listview);
+//        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                String selected_name = arr.get(position).getName();
+//                Toast.makeText(DisplayContactListActivity.this, "You selected " + selected_name, Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(DisplayContactListActivity.this, TimerActivity.class);
+//                intent.putExtra("Selected_name", selected_name);
+//                intent.putExtra("option", getIntent().getStringExtra("option"));
+//                startActivity(intent);
+
+//            }
+//        });
     }
 }
